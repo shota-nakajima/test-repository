4874762eb796df7f30266f56123284098d8a198f
shota-nakajima <diginarublerebo@yahoo.co.jp>
2025-01-23 20:02:00 +0900

    mult関数を追加

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ function add(a, b) {
   return a + b;
 }
 
-console.log(add(10, 55));
-
 function sub(a, b) {
   return a - b;
 }
 
-console.log(sub(55, 10));
+function mult(a, b) {
+  return a * b;
+}
+
+console.log(mult(5, 10));
